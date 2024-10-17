32ec530ebb10e041fa99735ce7b0587c37a375b9
AryaGGWP <arya.lampung.2016@gmail.com>
2024-10-17 19:33:23 +0700

    mengubah resource gambar dan sedikit memofifikasi dengan membuat funsi heartshape dan memanggilnya ke dalam modifier clip.

--- app/src/main/java/com/example/pertemuan_3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan_3/MainActivity.kt
@@ -13,26 +13,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pertemuan_3.ui.theme.Pertemuan_3Theme
+import androidx.compose.ui.graphics.Shape
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,19 +59,17 @@ fun BasicCompose(arya: Modifier = Modifier){
             fontSize = 50.sp, color = Color.Black,
             fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(5.dp))
-
-        Text(text = "Ini adalah Halaman Login", modifier = arya,
+        Text(text = "Ini adalah Halaman Login",
             fontSize = 15.sp, color = Color.Gray)
 
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(5.dp))
 
         Image(painter = painterResource(id = R.drawable.umy),
             contentDescription = null,
-            modifier = arya.size(200.dp)
+            modifier = arya.size(300.dp)
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
         Text(text = "Nama",
             fontSize = 20.sp, color = Color.Black,
@@ -93,15 +89,29 @@ fun BasicCompose(arya: Modifier = Modifier){
             fontSize = 26.sp,
             color = Color.Black)
 
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(10.dp))
 
-        Image(painter = painterResource(id = R.drawable.fotosaya),
+        Image(painter = painterResource(id = R.drawable.yak),
             contentDescription = null,
-            modifier = arya.size(350.dp).clip(RoundedCornerShape(50.dp))
-                .border(8.dp, Color.Gray, RoundedCornerShape(50.dp))
+            modifier = arya
+                .size(350.dp)
+                .clip(heartshape())
+                .border(5.dp, Color.Blue, heartshape())
                 .fillMaxSize(),
             contentScale = ContentScale.Crop
         )
+    }
+}
+
+fun heartshape(): Shape {
+    return GenericShape { size, _ ->
+        val width = size.width
+        val height = size.height
+
+        moveTo(width / 2, height * 0.3f)
+        cubicTo(0f, 0f, 0f,height * 0.6f, width / 2, height)
+        cubicTo(width, height * 0.6f, width, 0f, width / 2, height * 0.3f)
+        close()
     }
 }
 
